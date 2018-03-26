6405bcaafe774a7d1231b4c442e64a831c399b3b
rinotsuka <rino.tsuka.1226@gmail.com>
2018-03-27 06:31:21 +0900

    コピーライト年号を修正

--- src/footer.php
+++ src/footer.php
@@ -2,7 +2,20 @@
   <div class="footer__body">
     <div class="copyright">
       <p><span class="copyright__site-name"><?php bloginfo('name'); ?></span><br>
-      &copy; 2018 <span class="copyright__name"><?php echo get_option('user_name_yomi'); ?></span></p>
+      &copy; <?php
+        $getlatest = get_posts('numberposts=1&post_type=any&orderby=modified');
+        foreach($getlatest as $post) {
+          if(get_the_date('Y')) {
+            if(get_the_date('Y') == date('Y')){
+              echo date('Y');
+            } else {
+              echo get_the_date('Y').'-'.date('Y');
+            }
+          } else {
+            echo get_the_date('Y');
+          }
+        }
+      ?> <span class="copyright__name"><?php echo get_option('user_name_yomi'); ?></span></p>
     </div>
     <div class="credit">
       <p>Powered by WordPress<br>
@@ -16,7 +29,7 @@
 <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/lazyload@2.0.0-beta.2/lazyload.js"></script>
 <script src="<?php bloginfo('template_url'); ?>/js/script.min.js"></script>
-<?php echo htmlspecialchars_decode(get_option('it_google_analytics')); ?>
+<?php if(get_option('it_google_analytics')) echo htmlspecialchars_decode(get_option('it_google_analytics')); ?>
 <?php wp_footer(); ?>
 </body>
 </html>
